c0a4f7ba206bb8c840b1e9bccc2699cfb4222f78
struo288 <78322463+simont-17@users.noreply.github.com>
2024-11-18 14:03:17 -0700

    added handleGetQuizzes

--- src/routes/quizzesRoutes.js
+++ src/routes/quizzesRoutes.js
@@ -2,8 +2,25 @@ const express = require('express')
 const router = express.Router();
 
 const handleGetQuizzes = async (req,res) => {
-    try{}
-    catch{}
+    try{
+        const connection = await conn();
+        const [rows] = await connection.execute('SELECT * From Quizzes');
+        await connection.end();
+        if (rows.length > 0) {
+            const { quiz_id, module_id, title } = rows[0];
+            res.status(200).json({
+                success: true,
+                message: `We found the following quiz with quiz ID ${id}`,
+                data: {quiz_id, module_id, title}
+            });
+        } else {
+            res.status(404).json({message: 'No quzzes have been found'})
+        }
+    }
+    catch(err){
+        console.error('An error occured while fetching all quizzes...', err.message);
+        res.status(500).json({message: 'A server error occured...'});
+    }
 }
 
 const handleGetQuizzesByID = async (req,res) => { // To be changed later when person responsible implements questions routes
