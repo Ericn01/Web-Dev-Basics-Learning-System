e0c0e64ff8cde07e44a35e1ef9c5770f66d15d1d
struo288 <78322463+simont-17@users.noreply.github.com>
2024-11-18 13:35:45 -0700

    created handleGetModuleById

--- src/routes/learningModulesRoutes.js
+++ src/routes/learningModulesRoutes.js
@@ -1,5 +1,7 @@
+const e = require('express');
 const express = require('express');
 const router = express.Router();
+const { conn } = require('../config/db')
 
 const handleGetModules = async (req,res) => {
     try {}
@@ -7,8 +9,29 @@ const handleGetModules = async (req,res) => {
 }
 
 const handleGetModulesByID = async (req,res) => {
-    try{}
-    catch{}
+    try{
+        let id = req.params.id;
+
+        const connection = await conn();
+        const [rows] = await connection.execute(
+            'SELECT * FROM Modules WHERE module_id = ?', [id]
+        );
+        await connection.end();
+        if (rows.length > 0) {
+            const { module_id, title, module_description, content, banner_image_path, created_at } = rows[0];
+            res.status(200).json({
+                success: true,
+                message: `We found the following module with module ID ${id}`,
+                data: {module_id, title, module_description, content, banner_image_path, created_at}
+            });
+        } else {
+            res.status(404).json({message: 'Module not found'})
+        }
+    }
+    catch(err){
+        console.error('An error occured while finding the module with the specified ID', err.message);
+        res.status(500).json({message: 'A server error occured...'});
+    }
 }
 
 router.get('/modules', handleGetModules);
