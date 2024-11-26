6699e140d86b6a4005ce29f843d42db27c9a5cda
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-11-25 23:14:38 -0700

    started the react project and created a prototype faq just to see if we can retrieve data

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// Creating the express application
-const express = require('express')
+const express = require('express');
+const cors = require('cors'); 
 const userAuthRoutes = require('./src/routes/userAuthRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 const learningModuleRoutes = require('./src/routes/learningModulesRoutes');
 const quizzesRoutes = require('./src/routes/quizzesRoutes');
 const FAQRoutes = require('./src/routes/userFAQRoutes');
-require('dotenv').config();
 
 const app = express();
+app.use(cors()); 
 
-app.use(express.json()); // middleware that helps with parsin requests.
+
+app.use(express.json());
 app.use('/webdev-learning/api', userAuthRoutes);
 app.use('/webdev-learning/api', userRoutes);
 app.use('/webdev-learning/api', learningModuleRoutes);
@@ -20,4 +21,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}. Listening for incoming HTTP requests...`);
 });
-
