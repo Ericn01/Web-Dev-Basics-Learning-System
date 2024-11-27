8623ffa288a10f871174d19911a415f5aa8cbec1
Ericn01 <nielseneric2001@gmail.com>
2024-11-27 15:31:16 -0700

    Created module display component (currently in app.js)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import FaqComponent from './components/FaqComponent'
+import ModulesPage from './components/ModuleComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <FaqComponent/>
+      <ModulesPage/>
     </>
   )
 }
