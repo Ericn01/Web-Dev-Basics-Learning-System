7d2056782b28222ac2bea795e05e8ce11e356e16
Ericn01 <nielseneric2001@gmail.com>
2024-11-28 23:11:52 -0700

    Another quick database config change

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -2,23 +2,22 @@
 const mysql = require('mysql2/promise')
 require('dotenv').config()
 
-// Setting up database config variables 
-
-// This is the one that will be used in product, but right now I just want to test with a local connection
-
+// Setting up the database configuration
 const dbConfig = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE, 
-    port: process.env.PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 }
 
 // Returning a database connection
-const connectToDB = mysql.createPool(dbConfig);
+const connectToDB = () => {
+    return mysql.createPool(dbConfig);
+}
 
 
 module.exports = { connectToDB };
