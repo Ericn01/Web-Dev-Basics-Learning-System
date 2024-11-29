adacb1883b76f154ac485e03010bddae211e533a
Ericn01 <nielseneric2001@gmail.com>
2024-11-28 22:43:27 -0700

    Db configuration now uses environment variables rather than hardcoded values.

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -5,23 +5,14 @@ require('dotenv').config()
 // Setting up database config variables 
 
 // This is the one that will be used in product, but right now I just want to test with a local connection
-/** 
+
 const dbConfig = {
-    host: process.env.HOST_IP,
+    host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE, 
     port: process.env.PORT,
 }
-*/
-
-// This will only work if the local mysql instance is running, the root password has been set to '12345', and the 'createdb.sql' file has been ran in mysql server.
-const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '12345',
-    database: 'WebDevLearning'
-};
 
 // Returning a database connection
 const connectToDB = async() => {
