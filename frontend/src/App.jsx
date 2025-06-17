320317fde515dd4ed6819a4951f88ac85d9ee5d7
Ericn01 <nielseneric2001@gmail.com>
2025-06-17 12:29:24 -0600

    Changed Router from BrowserRouter to HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './services/authContext';
 import { Login, Signup, AdminRoute, ProtectedRoute } from './components/Authentication';
 import Layout from './components/Layout';
